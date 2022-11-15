893b2bb138984b0db986f85cc44db39d7ad2101f
Daniel Izevbije <dizevbije94@gmail.com>
2022-11-15 13:02:35 +0100

    first commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+const WHITELIST_CONTRACT_ADDRESS = "0xBAE00e8F748eD088F3D474D313ea5b814fDBfb2F";
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
